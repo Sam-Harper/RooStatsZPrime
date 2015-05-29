26716f4924187c840e487cb829f6f0b5a06f44f8
Sam Harper <sam.j.harper@gmail.com>
2015-05-29 15:04:04 +0100

    inital commit into new format, does not yet compile

---
+++ Nspectra/packages/RooStatsZPrime/package.mk
@@ -0,0 +1,16 @@
+PKG_OBJ_DIR=$(OBJ_DIR)/RooStatsZPrime/src
+
+ROOSTATSZPRIME_LIBFILES        =$(PKG_OBJ_DIR)/DataBox.o $(PKG_OBJ_DIR)/DataPruner.o $(PKG_OBJ_DIR)/ModelConfigurator.o $(PKG_OBJ_DIR)/ModelConfiguratorZprime.o $(PKG_OBJ_DIR)/Pixie.o $(PKG_OBJ_DIR)/PoiRangeEstimator.o $(PKG_OBJ_DIR)/Resultator.o $(PKG_OBJ_DIR)/ResultatorWithBAT.o 
+
+ROOSTATSZPRIME_LIBNAME 	= $(LIB_DIR)/libRooStatsZPrime.so
+
+$(ROOSTATSZPRIME_LIBNAME):	$(ROOSTATSZPRIME_LIBFILES)
+		@$(LD) $(SOFLAGS) $(LDFLAGS) $(LIBS) $^ -o $@
+		@echo "$@ done"
+
+RooStatsZPrime/src/%Dict.cc: PACKAGENAME:= RooStatsZPrime
+RooStatsZPrime/src/%_LinkDef.h:   packages/RooStatsZPrime/dict/%_LinkDef.h packages/RooStatsZPrime/include/%.hh
+#	@echo "joy and happyness $*"
+	@sleep 0.001s #dummy command, okay its been a long battle and this hack works... 
+
+STD_LIBS	+= $(ROOSTATSZPRIME_LIBNAME)
